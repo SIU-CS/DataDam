452fdc39e90b9ffaffb3d394175c5df879ce3adb
Jackson <jones.jackson13@siu.edu>
2017-02-26 11:29:51 -0600

    Prototype User Interface

---
+++ DataDam/app/src/main/java/com/example/jackson/datadam/HomeActivity.java
@@ -0,0 +1,19 @@
+package com.example.jackson.datadam;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+}
