b964520ba6261a2b0e00947fadaa82cdf143aa3b
bshultz1991 <brandt.shultz@siu.edu>
2017-03-01 21:58:57 -0600

    Altered code to fit working system

--- DataDam/app/src/main/java/com/example/jackson/datadam/HomeActivity.java
+++ DataDam/app/src/main/java/com/example/jackson/datadam/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.jackson.datadam;
 
 import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -20,15 +21,20 @@ import java.util.ArrayList;
         import android.net.TrafficStats;
         import android.os.Bundle;
         import android.os.Handler;
-import android.widget.ListView;
-import android.widget.TextView;
+        import android.widget.ListView;
+        import android.widget.TextView;
         import android.app.ActivityManager;
         import android.net.NetworkInfo;
         import android.content.Context;
         import android.app.ActivityManager.RunningAppProcessInfo;
         import android.net.ConnectivityManager;
-        import java.util.List;
-        import 	android.app.ListActivity;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import 	android.app.ListActivity;
 class Application{
     private String name;
     private int uid;
@@ -54,26 +60,156 @@ class Application{
     public long getBytes(){
         return bytes;
     }
+    public String getName(){return name;}
 }
 public class HomeActivity extends Activity {
     private Handler mHandler = new Handler();
     private long PreviousRX = 0;
     private long PreviousTX= 0;
     private long currentRX, currentTX, rxBytes, txBytes;
-    ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+    ActivityManager manager;
+
+    ConnectivityManager connectMgn;
     List<RunningAppProcessInfo> runningapplications;
-    List<Application>applications;
+    List<Application>applications= new List<Application>() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean contains(Object o) {
+            return false;
+        }
+
+        @NonNull
+        @Override
+        public Iterator<Application> iterator() {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public Object[] toArray() {
+            return new Object[0];
+        }
+
+        @NonNull
+        @Override
+        public <T> T[] toArray(T[] ts) {
+            return null;
+        }
+
+        @Override
+        public boolean add(Application application) {
+            return false;
+        }
+
+        @Override
+        public boolean remove(Object o) {
+            return false;
+        }
+
+        @Override
+        public boolean containsAll(Collection<?> collection) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(Collection<? extends Application> collection) {
+            return false;
+        }
+
+        @Override
+        public boolean addAll(int i, Collection<? extends Application> collection) {
+            return false;
+        }
+
+        @Override
+        public boolean removeAll(Collection<?> collection) {
+            return false;
+        }
+
+        @Override
+        public boolean retainAll(Collection<?> collection) {
+            return false;
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public Application get(int i) {
+            return null;
+        }
+
+        @Override
+        public Application set(int i, Application application) {
+            return null;
+        }
+
+        @Override
+        public void add(int i, Application application) {
+
+        }
+
+        @Override
+        public Application remove(int i) {
+            return null;
+        }
+
+        @Override
+        public int indexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public int lastIndexOf(Object o) {
+            return 0;
+        }
+
+        @Override
+        public ListIterator<Application> listIterator() {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public ListIterator<Application> listIterator(int i) {
+            return null;
+        }
+
+        @NonNull
+        @Override
+        public List<Application> subList(int i, int i1) {
+            return null;
+        }
+    };
     private TextView RX;
     private TextView TX;
+    private TextView HighestName;
+    private TextView HighestValue;
 
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
         PreviousRX = TrafficStats.getTotalRxBytes();
         PreviousTX = TrafficStats.getTotalTxBytes();
-        Applicationupdate();
         RX = (TextView) findViewById(R.id.RX);
         TX = (TextView) findViewById(R.id.TX);
+        HighestName= (TextView) findViewById(R.id.HN);
+        HighestValue= (TextView) findViewById(R.id.HV);
+        manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        connectMgn= (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        Applicationupdate(manager);
 
         if (PreviousRX== TrafficStats.UNSUPPORTED || PreviousTX == TrafficStats.UNSUPPORTED) {
             AlertDialog.Builder alert = new AlertDialog.Builder(this);
@@ -87,7 +223,7 @@ public class HomeActivity extends Activity {
 
     private final Runnable mRunnable = new Runnable() {
         public void run() {
-            Applicationupdate();
+            Applicationupdate(manager);
             long totalbytes= TrafficStats.getTotalRxBytes();
             currentRX = totalbytes - PreviousRX;
             PreviousRX= totalbytes;
@@ -96,42 +232,44 @@ public class HomeActivity extends Activity {
             PreviousTX= totalbytes;
             //If the device is offline, Data Dam records the data to the bytes the device has used offline.
             //Otherwise the system updates the flags to prepare for the next runnable cycle.
-            if(!isOnline()) {
+//            if(!isOnline(connectMgn)) {
                 rxBytes = rxBytes + currentRX;
                 txBytes= txBytes + currentTX;
                 RX.setText(Long.toString(rxBytes));
                 TX.setText(Long.toString(txBytes));
                 for(Application application: applications){
-                    int uid = application.getUid();
+                  int uid = application.getUid();
                     long bytes= TrafficStats.getUidRxBytes(uid)+TrafficStats.getUidTxBytes(uid);
                     application.addBytes(bytes);
                 }
-            }
-            else{
+            Application highest = HighestUsingApplication();
+            HighestName.setText(highest.getName());
+            HighestValue.setText(Long.toString(highest.getBytes()));
+//            }
+            /*else{
                 for(Application application: applications){
                     int uid = application.getUid();
                     long bytes= TrafficStats.getUidRxBytes(uid)+TrafficStats.getUidTxBytes(uid);
                     application.updatePrevious(bytes);
                 }
-            }
+            }*/
             mHandler.postDelayed(mRunnable, 1000);
         }
     };
 
     // Checks to see if the device is online with ConnectivityManager
     // Returns true if network info exists and the phone is connected to a network
-    public boolean isOnline(){
-        ConnectivityManager connectMgn = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectMgn.getActiveNetworkInfo();
-        return (networkInfo != null && networkInfo.isConnected());
-
-    }
+//    public boolean isOnline(ConnectivityManager connectMgn){
+//
+//        NetworkInfo networkInfo = connectMgn.getActiveNetworkInfo();
+//        return (networkInfo != null && networkInfo.isConnected());
+//
+//    }
 // Checks for any new applications and adds them to a permanent list
 // If list is empty, the currently running applications form a new list
-    private void Applicationupdate(){
+    private void Applicationupdate(ActivityManager manager){
         runningapplications= manager.getRunningAppProcesses();
-        if(applications == null){
+        if(applications.isEmpty()){
             for(RunningAppProcessInfo runningapplication : runningapplications){
                 int uid = runningapplication.uid;
                 String name=runningapplication.processName;
